1759db9dde33db48277131a38d220dd8784a107f
goui <guillaume.galliano@gmail.com>
2016-12-15 07:12:19 -0500

    Created spring boot project

---
+++ src/main/java/fr/goui/gouirganizer/GouirganizerApplication.java
@@ -0,0 +1,12 @@
+package fr.goui.gouirganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GouirganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GouirganizerApplication.class, args);
+	}
+}
---
+++ src/test/java/fr/goui/gouirganizer/GouirganizerApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.goui.gouirganizer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GouirganizerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

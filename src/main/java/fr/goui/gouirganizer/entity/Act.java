547a575e1bb4abf0816942a36165d4e77f7192d2
goui <guillaume.galliano@gmail.com>
2016-12-15 17:56:19 -0500

    Added Act entity

---
+++ src/main/java/fr/goui/gouirganizer/entity/Act.java
@@ -0,0 +1,76 @@
+package fr.goui.gouirganizer.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+/**
+ * Entity representing an act of work.
+ */
+@Entity
+public class Act {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int duration;
+
+    @Column(nullable = false)
+    private int price;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Act act = (Act) o;
+        return id == act.id &&
+                duration == act.duration &&
+                price == act.price &&
+                Objects.equals(name, act.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, duration, price);
+    }
+}
